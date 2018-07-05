b54f2b0b11fd69449d1821d0d1970458c5c939af
Pa Yeng Thao <payengt24@gmail.com>
2018-07-05 14:15:31 -0500

    updated marker

--- src/components/MapView/MapContainer/MapContainer.js
+++ src/components/MapView/MapContainer/MapContainer.js
@@ -21,79 +21,18 @@ class MapContainer extends Component {
         };
     }
 
-    //loop through marker and assigning to a photog
-    getMarker = (photog) => {
-        const markerImg = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', '12.png', '13.png', '14.png', '15.png',]
-        if (photog.toLowerCase() == 'photog 1') {
-            return markerImg[0];
-        } else if (photog == 'photog 2') {
-            return markerImg[1];
-        } else if (photog == 'photog 3') {
-            return markerImg[2];
-        } else if (photog == 'photog 4') {
-            return markerImg[3];
-        } else if (photog == 'photog 5') {
-            return markerImg[4];
-        } else if (photog == 'photog 6') {
-            return markerImg[5];
-        } else if (photog == 'photog 7') {
-            return markerImg[6];
-        } else if (photog == 'photog 8') {
-            return markerImg[7];
-        } else if (photog == 'photog 9') {
-            return markerImg[8];
-        } else if (photog == 'photog 10') {
-            return markerImg[9];
-        } else if (photog == 'photog 11') {
-            return markerImg[10];
-        } else if (photog == 'photog 12') {
-            return markerImg[11];
-        } else if (photog == 'photog 13') {
-            return markerImg[12];
-        } else if (photog == 'photog 14') {
-            return markerImg[13];
-        } else if (photog == 'photog 15') {
-            return markerImg[14];
-        }
-    }
-
-    
-
-
-
-
     render() {
 
-        // // EXTRACT CALENDAR FROM THIRD-PARTY SCHEDULING APPLICATION AS RESOURCE FOR THIS APPLICATION'S SCHEDULE DISPLAY
-        // const resourceExtractor = originalObject => {
-        //     let extractedResource = originalObject.calendar;
-        //     return extractedResource;
-        // }
-        // const resourceArray = originalAcuityArray.map(resourceExtractor);
-        // const uniqueResourcesArray = [...new Set(resourceArray)];
-        // const resourceList = uniqueResourcesArray.map(currentResource => {
-        //     return {
-        //         id: currentResource,
-        //         title: currentResource
-        //     }
-        // });
-        // // END EXTRACT CALENDAR FROM THIRD-PARTY-SCHEDULING APPLICAITON AS RESOURCE FOR THIS APPLICATION'S SCHEDULE DISPLAY
-
-
-
         let appointments = this.props.reduxState.mapData.mapData;
-
         let photogs = appointments.map(((photog) => {
             return photog.calendar
         }));
 
-
-
         console.log('ttttttttt', photogs);
 
-        let placeDisplayOnMarker = appointments.map(((appointment) => {
+        let placeDisplayOnMarker = appointments.map(((appointment, i) => {
             return (
-                <Marker key={appointment._id} position={{ lat: appointment.lat, lng: appointment.lng }} icon={this.getMarker(appointment.calendar)} />
+                <Marker key={appointment._id} position={{ lat: appointment.lat, lng: appointment.lng }} icon={(i ++) + '.png' } />
             )
         }));
 
--- src/components/MapView/Mileage/Mileage.js
+++ src/components/MapView/Mileage/Mileage.js
@@ -11,46 +11,13 @@ class Mileage extends Component {
         super(props);
     }
 
-    //loop through marker and assigning to a photog
-    getMarker = (photog) => {
-        const markerImg = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', '12.png', '13.png', '14.png', '15.png',]
-        if (photog.toLowerCase() == 'photog 1') {
-            return markerImg[0];
-        } else if (photog == 'photog 2') {
-            return markerImg[1];
-        } else if (photog == 'photog 3') {
-            return markerImg[2];
-        } else if (photog == 'photog 4') {
-            return markerImg[3];
-        } else if (photog == 'photog 5') {
-            return markerImg[4];
-        } else if (photog == 'photog 6') {
-            return markerImg[5];
-        } else if (photog == 'photog 7') {
-            return markerImg[6];
-        } else if (photog == 'photog 8') {
-            return markerImg[7];
-        } else if (photog == 'photog 9') {
-            return markerImg[8];
-        } else if (photog == 'photog 10') {
-            return markerImg[9];
-        } else if (photog == 'photog 11') {
-            return markerImg[10];
-        } else if (photog == 'photog 12') {
-            return markerImg[11];
-        } else if (photog == 'photog 13') {
-            return markerImg[12];
-        } else if (photog == 'photog 14') {
-            return markerImg[13];
-        } else if (photog == 'photog 15') {
-            return markerImg[14];
-        }
-    }
-
     render() {
+
+        // console.log('-------cool', this.getMarker());
         let appointments = this.props.reduxState.mapData.mapData;
-        let photogs = appointments.map(((appointment) => {
-            return <tr><td><img src={this.getMarker(appointment.calendar)} />{appointment.calendar}</td><td>{appointment.travel_distance} miles</td></tr>
+
+        let photogs = appointments.map(((appointment, i) => {
+            return <tr><td><img src={(i ++) + '.png' } />{appointment.calendar}</td><td>{appointment.travel_distance} miles</td></tr>
         }));
 
         return (
